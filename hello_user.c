6591b479c3695bb2b02ab3cb5cada6b137de9052
alex-wong123 <huanying0003301@163.com>
2021-10-13 10:44:58 +0800

    Create hello_user.c

---
+++ hello_user.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "bpf_load.h"
+#include "trace_helpers.h"
+
+int main(int argc, char **argv)
+{
+ if( load_bpf_file("hello_kern.o") != 0)
+ {
+  printf("The kernel didn't load BPF program\n");
+  return -1;
+ }
+
+ read_trace_pipe();
+ return 0;
+}
